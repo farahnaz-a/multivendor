4d255251fb71167b0df752b80aca684083b9242d
maksudur-rhmn <john.alim45@gmail.com>
2021-05-18 01:42:26 +0600

    project initialization

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+
+
+    <input type="text" placeholder="Enter name">
+    <input type="text" placeholder="Enter address">
+    <input type="text" placeholder="Enter phone">
+    <input type="submit">
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [FrontendController::class, 'index']);
+
+Route::get('/about', [FrontendController::class, 'about'])->middleware(['auth', 'verified']); 
+
+Route::get('/contact', [FrontendController::class, 'contact']);
+
+
+
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
